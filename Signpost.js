6ee550f74c587e3dc67677eb4bb66536e57839f2
snowlong <surugay@mode2.local>
2016-09-15 11:23:59 +0900

    add method for ie9 not defined console

--- Signpost.js
+++ Signpost.js
@@ -16,6 +16,29 @@ window.Signpost = {
     this.level = level || this.LEVEL.FULL;
     this.avoidError();
   },
+  avoidError: function() {
+    // For IE9, NOT defined console.
+    // So avoid error, define console methods.
+    if(typeof console === "undefined") {
+      var console = {
+        debug: function(msg) {
+          ;
+        },
+        info: function(msg) {
+          ;
+        },
+        log: function(msg) {
+          ;
+        },
+        warn: function(msg) {
+          ;
+        },
+        error: function(msg) {
+          ;
+        }
+      };
+    }
+  },
   debug: function(msg) {
     (this.level >= this.LEVEL.DEBUG) && console.debug(msg);
   },
